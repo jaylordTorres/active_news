43bd34eef706e18695855216ea5c2ead252ac500
Jay Lord P. Torres <isens.jaylord@gmail.com>
2020-12-06 22:12:24 +0800

    dummy data

---
+++ lib/data/main.dart
@@ -0,0 +1,60 @@
+import '../shared_class/main.dart';
+
+final List<FeedModel> feeds = [
+  FeedModel(
+      source: SourceModel(id: null, name: "FXStreet"),
+      author: "John Isige",
+      title:
+          "Bitcoin Price Prediction: BTC looks poised for a 16% downswing to \$15,500 before recovery occurs",
+      description:
+          'Bitcoin has bagged \$19,000 again but failed to overcome the resistance at \$19,200. The most critical level at \$19,500 was not touched. Meanwhile, BTC',
+      url:
+          "https://www.fxstreet.com/cryptocurrencies/news/bitcoin-price-prediction-btc-looks-poised-for-a-16-downswing-to-15-500-before-recovery-occurs-202012060910",
+      urlToImage:
+          "https://editorial.fxstreet.com/images/Markets/Currencies/Digital Currencies/Bitcoin/bit-coin-symbol-flag-3d-illustration-59931110_Large.jpg",
+      publishedAt: "2020-12-06T09:10:23Z",
+      content:
+          "<ul><li>Bitcoin is fighting tooth and nail to hold above \$19,000 and avoid possible losses to \$15,500.</li><li>The reduction in network growth since the beginning of September helps to validate the d… [+1723 chars]"),
+  FeedModel(
+      source: SourceModel(id: null, name: "FXStreet"),
+      author: "John Isige",
+      title:
+          "Bitcoin Price Prediction: BTC looks poised for a 16% downswing to \$15,500 before recovery occurs",
+      description:
+          'Bitcoin has bagged \$19,000 again but failed to overcome the resistance at \$19,200. The most critical level at \$19,500 was not touched. Meanwhile, BTC',
+      url:
+          "https://www.fxstreet.com/cryptocurrencies/news/bitcoin-price-prediction-btc-looks-poised-for-a-16-downswing-to-15-500-before-recovery-occurs-202012060910",
+      urlToImage:
+          "https://editorial.fxstreet.com/images/Markets/Currencies/Digital Currencies/Bitcoin/bit-coin-symbol-flag-3d-illustration-59931110_Large.jpg",
+      publishedAt: "2020-12-06T09:10:23Z",
+      content:
+          "<ul><li>Bitcoin is fighting tooth and nail to hold above \$19,000 and avoid possible losses to \$15,500.</li><li>The reduction in network growth since the beginning of September helps to validate the d… [+1723 chars]"),
+  FeedModel(
+      source: SourceModel(id: null, name: "FXStreet"),
+      author: "John Isige",
+      title:
+          "Bitcoin Price Prediction: BTC looks poised for a 16% downswing to \$15,500 before recovery occurs",
+      description:
+          'Bitcoin has bagged \$19,000 again but failed to overcome the resistance at \$19,200. The most critical level at \$19,500 was not touched. Meanwhile, BTC',
+      url:
+          "https://www.fxstreet.com/cryptocurrencies/news/bitcoin-price-prediction-btc-looks-poised-for-a-16-downswing-to-15-500-before-recovery-occurs-202012060910",
+      urlToImage:
+          "https://editorial.fxstreet.com/images/Markets/Currencies/Digital Currencies/Bitcoin/bit-coin-symbol-flag-3d-illustration-59931110_Large.jpg",
+      publishedAt: "2020-12-06T09:10:23Z",
+      content:
+          "<ul><li>Bitcoin is fighting tooth and nail to hold above \$19,000 and avoid possible losses to \$15,500.</li><li>The reduction in network growth since the beginning of September helps to validate the d… [+1723 chars]"),
+  FeedModel(
+      source: SourceModel(id: null, name: "FXStreet"),
+      author: "John Isige",
+      title:
+          "Bitcoin Price Prediction: BTC looks poised for a 16% downswing to \$15,500 before recovery occurs",
+      description:
+          'Bitcoin has bagged \$19,000 again but failed to overcome the resistance at \$19,200. The most critical level at \$19,500 was not touched. Meanwhile, BTC',
+      url:
+          "https://www.fxstreet.com/cryptocurrencies/news/bitcoin-price-prediction-btc-looks-poised-for-a-16-downswing-to-15-500-before-recovery-occurs-202012060910",
+      urlToImage:
+          "https://editorial.fxstreet.com/images/Markets/Currencies/Digital Currencies/Bitcoin/bit-coin-symbol-flag-3d-illustration-59931110_Large.jpg",
+      publishedAt: "2020-12-06T09:10:23Z",
+      content:
+          "<ul><li>Bitcoin is fighting tooth and nail to hold above \$19,000 and avoid possible losses to \$15,500.</li><li>The reduction in network growth since the beginning of September helps to validate the d… [+1723 chars]"),
+];
