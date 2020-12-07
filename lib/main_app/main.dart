8eb85319d10b89ebd8ff7aac66a2316ddb19030f
Jay Lord P. Torres <jaylord@umpisa.co>
2020-12-07 22:47:05 +0800

    enable script load on app

--- lib/main_app/main.dart
+++ lib/main_app/main.dart
@@ -1,17 +1,46 @@
-import 'package:active_news/constant/route.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../constant/route.dart';
+import '../shared_class/main.dart';
 import 'app_page.dart';
 import 'theme_data.dart';
 
 /// Main app wrapper
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({Key key}) : super(key: key);
 
   @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  bool _initialized = false;
+  bool _activated = false;
+
+  @override
+  void initState() {
+    _initApp();
+    super.initState();
+  }
+
+  void _initApp() async {
+    final storage = await SharedPreferences.getInstance();
+    Store.storage = storage;
+    setState(() {
+      _initialized = true;
+      _activated = storage.getBool("activated") ?? false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (_initialized == false) {
+      return Container();
+    }
     return MaterialApp(
       theme: themeData,
-      initialRoute: uiRoutes[UiRoutes.feeds],
+      initialRoute: uiRoutes[_activated ? UiRoutes.feeds : UiRoutes.activation],
       routes: uiRoutePages,
     );
   }
